f1e73690929da49dc702ed4af48b5c0f0d31395a
Masaomi419 <84972948+Masaomi419@users.noreply.github.com>
2021-07-20 17:18:20 +0900

    課題提出
    オーバーロード

---
+++ JavaStudy/2-3/src/study/Calculator.java
@@ -0,0 +1,24 @@
+package study;
+
+
+		// TODO 自動生成されたメソッド・スタブ
+
+public class Calculator {
+
+		    protected int plus(int a) {
+		        return a + 1;
+		    }
+
+		    protected int plus(int a, int b) {
+		        return a + b;
+		    }
+
+		    protected int plus(int a, int b, int c) {
+		        return a + b + c;
+		    }
+
+		
+
+	}
+
+
